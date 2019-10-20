b06086cfdfd00d4c7252bd9ca8041eef83f6932f
creazybird <2633411459@qq.com>
2019-10-20 15:22:24 +0800

    fix

--- vue_web/config/index.js
+++ vue_web/config/index.js
@@ -8,11 +8,11 @@ module.exports = {
   dev: {
 
     // Paths
-    // assetsSubDirectory: 'static',
-    // assetsPublicPath: '/',
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/',
     proxyTable: {
         '/api':{
-            target:'http://121.196.202.46:6000',
+            target:'http://121.196.202.46',
             changeOrigin:true,
             pathRewrite:{
               '^/api':'/api'
@@ -22,7 +22,7 @@ module.exports = {
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
-    port: 4000, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
@@ -56,7 +56,7 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
 
     // Paths
-    assetsRoot: path.rlesolve(__dirname, '../dist'),
+    assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: './',
 
